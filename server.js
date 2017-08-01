c3f5ffa2d8e9dc5801d02b20fe10c4cfb181cf20
mc4tech <chavezmeguel@yahoo.com>
2017-08-01 19:35:34 -0400

    initial

---
+++ server.js
@@ -0,0 +1,42 @@
+// Here we require/import the HTTP module
+var http = require("http");
+
+//these are the ports the server listens to 
+var PORTONE = 7000;
+var PORTTWO = 7500;
+
+// Here we create a generic function to handle requests and responses
+function handleRequestOne(request, response) {
+	console.log("Good job!");
+  // The below statement is triggered (client-side) when the user visits the PORT URL
+  response.end("Good job!!! Path Hit: " + request.url);
+}
+
+// Here we create a generic function to handle requests and responses
+function handleRequestTwo(request, response) {
+	console.log("Catch up sucka!");
+  // The below statement is triggered (client-side) when the user visits the PORT URL
+  response.end("Catch up sucka! Path Hit: " + request.url);
+}
+
+// Here we use the Node HTTP package to create our server.
+// We then pass it the handleRequest function to empower it with functionality.
+var serverOne = http.createServer(handleRequestOne);
+var serverTwo = http.createServer(handleRequestTwo);
+
+// Here we start our server so that it can begin listening to client requests.
+serverOne.listen(PORTONE, function() {
+
+  // The below statement is triggered (server-side) when a user visits the PORT URL
+  console.log("Server listening on: http://localhost:%s", PORTONE);
+
+});
+
+// Here we start our server so that it can begin listening to client requests.
+serverTwo.listen(PORTTWO, function() {
+
+  // The below statement is triggered (server-side) when a user visits the PORT URL
+  console.log("Server listening on: http://localhost:%s", PORTTWO);
+
+});
+
